5ad0c6eabc2fbd879d8fb16f3c009eb9494599a7
DiegoPama <105394232+DiegoPama01@users.noreply.github.com>
2022-11-14 03:40:27 +0100

    No image placeholder

--- src/app/movies/services/movies.service.ts
+++ src/app/movies/services/movies.service.ts
@@ -14,15 +14,15 @@ export class MoviesService {
   constructor(private http: HttpClient) {}
 
   getMovies() { 
-    return this.http.get<SearchPetition>(`${this.baseUrl}?s=Matrix&apikey=4c5e476e`);
+    return this.http.get<SearchPetition>(`${this.baseUrl}?s=Matrix&type=movie&apikey=4c5e476e`);
   }
 
   getMoviesByID(id:String) { 
-    return this.http.get<Movie>(`${this.baseUrl}?i=${id}&apikey=4c5e476e`);
+    return this.http.get<Movie>(`${this.baseUrl}?i=${id}&type=movie&apikey=4c5e476e`);
   }
 
   getMoviesByName(name:String) { 
-    return this.http.get<SearchPetition>(`${this.baseUrl}?s=${name}&apikey=4c5e476e`);
+    return this.http.get<SearchPetition>(`${this.baseUrl}?s=${name}&type=movie&apikey=4c5e476e`);
   }
 
 }
