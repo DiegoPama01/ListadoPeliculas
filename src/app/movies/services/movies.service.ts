b4d1e6c3b84fbc5c56b1b33a8411210c1ab451e2
DiegoPama <105394232+DiegoPama01@users.noreply.github.com>
2022-11-15 11:34:06 +0100

    Más ejemplos

--- src/app/movies/services/movies.service.ts
+++ src/app/movies/services/movies.service.ts
@@ -1,34 +1,44 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { SearchPetition } from '../interfaces/searchPetition.interface';
-import { Movie } from '../interfaces/movies.interface';
 import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Movie } from '../interfaces/movies.interface';
+import { SearchPetition } from '../interfaces/searchPetition.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
-
-
-  private busquedaEjemplo: string[] = ["Matrix", "Iron Man", "Toy Story", "Superman", "Spiderman"] 
+  private busquedaEjemplo: string[] = [
+    'Matrix',
+    'Iron Man',
+    'Toy Story',
+    'Superman',
+    'Spiderman',
+    'Evangelion',
+  ];
 
   private baseUrl: string = environment.baseUrl;
 
-
-
   constructor(private http: HttpClient) {}
 
-  getMovies() { 
-    let number: number = Math.floor(Math.random()*this.busquedaEjemplo.length)
-    return this.http.get<SearchPetition>(`${this.baseUrl}?s=${this.busquedaEjemplo[number]}&type=movie&apikey=4c5e476e`);
+  getMovies() {
+    let number: number = Math.floor(
+      Math.random() * this.busquedaEjemplo.length
+    );
+    return this.http.get<SearchPetition>(
+      `${this.baseUrl}?s=${this.busquedaEjemplo[number]}&type=movie&apikey=4c5e476e`
+    );
   }
 
-  getMoviesByID(id:String) { 
-    return this.http.get<Movie>(`${this.baseUrl}?i=${id}&type=movie&apikey=4c5e476e`);
+  getMoviesByID(id: String) {
+    return this.http.get<Movie>(
+      `${this.baseUrl}?i=${id}&type=movie&apikey=4c5e476e`
+    );
   }
 
-  getMoviesByName(name:String) { 
-    return this.http.get<SearchPetition>(`${this.baseUrl}?s=${name}&type=movie&apikey=4c5e476e`);
+  getMoviesByName(name: String) {
+    return this.http.get<SearchPetition>(
+      `${this.baseUrl}?s=${name}&type=movie&apikey=4c5e476e`
+    );
   }
-
 }
